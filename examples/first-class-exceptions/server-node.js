f47f425c158e0042eaeeaa904f63397b4e4c2830
ysimonson <yusuf@dotcloud.com>
2012-06-21 10:38:14 -0700

    Placed comment on newline to fix formatting on FF

--- examples/first-class-exceptions/server-node.js
+++ examples/first-class-exceptions/server-node.js
@@ -3,8 +3,9 @@ var zerorpc = require("zerorpc");
 var server = new zerorpc.Server({
     bad: function(reply) {
         //The first parameter to reply() is for errors
+        //Also possible:
+        //  reply(new Error(":P"), null, false);
         reply(":P", null, false);
-        //Also possible: reply(new Error(":P"), null, false);
     }
 });
 
